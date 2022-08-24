dc476df1b094be8a59021663a777550df29dea0b
Michal Jadach <michal.jadach@hoprnet.org>
2022-08-24 15:49:30 +0200

    Win info through HOPR

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { getPeerId, sendMessage } from '../functions/hopr-sdk'
 export default function Home() {  
   const router = useRouter();
   
-  const [win, setWin] = useState(null);
+  const [win, set_win] = useState(null);
   const [lobby, set_lobby] = useState(true);
   const [lobbyId, set_lobbyId] = useState(null);
   const childFunc = React.useRef(null);
@@ -28,6 +28,8 @@ export default function Home() {
   const [map, set_map] = useState(false);
 
   const [gotHoprAPI, set_gotHoprAPI] = useState(false);
+  const [startedAt, set_startedAt] = useState();
+  const [wonAt, set_wonAt] = useState([]);
 
   const [remotePos1, set_remotePos1] = useState([0,1]);
   const [remotePos2, set_remotePos2] = useState([0,1]);
@@ -41,9 +43,20 @@ export default function Home() {
   //   set_gotHoprAPI(true);
   // }, []);
 
-  useEffect(()=>{
-    console.log('players', players)
-  }, [players]);
+  //DEV WIN
+  // useEffect(()=>{
+  //   set_lobby(false)
+  //   set_peerId('16Uiu2HAkwrugLw54kVtDB4x9AULNv7kVqVkP5PAYVQwbNT4fJmpQ')
+  //   set_win('16Uiu2HAkwrugLw54kVtDB4x9AULNv7kVqVkP5PAYVQwbNT4fJmpQ')
+  //   set_wonAt([
+  //     {wonAt: 1234, peerId: '16Uiu2HAkwrugLw54kVtDB4x9AULNv7kVqVkP5PAYVQwbNT4fJmpQ'}
+  //   ])
+  // }, []);
+
+
+  // useEffect(()=>{
+  //   console.log('players', players)
+  // }, [players]);
 
   useEffect(()=>{
     if(!router.isReady) return;
@@ -94,6 +107,35 @@ export default function Home() {
   function startGame(){
     console.log('Index: startGame')
     set_lobby(false);
+    set_startedAt(Date.now())
+  }
+
+  function winGame(input){
+    set_win(input);
+    const wonAt = Date.now() - startedAt;
+    let message = {
+      from: hopr.current.peerId,
+      wonAt
+    }
+    for (let i = 0; i < players.length; i++) {
+      if(players[i].peerId !== peerId) {
+        sendMessage(hopr.current.apiEndpoint, hopr.current.apiToken, players[i].peerId, JSON.stringify(message));
+      }
+    }
+    let wonAtTmp = JSON.parse(JSON.stringify(wonAt));
+    wonAtTmp.push({wonAt, peerId: hopr.current.peerId})
+    set_wonAt(wonAtTmp);
+  }
+
+  function gotWsMessage(input){
+    let message = JSON.parse(input);
+    if(message.postion){
+      set_remotePos1(message.postion);
+    } else if (message.wonAt) {
+      let wonAtTmp = JSON.parse(JSON.stringify(wonAt));
+      wonAtTmp.push({wonAt: message.wonAt, peerId: message.from})
+      set_wonAt(wonAtTmp);
+    }
   }
 
   function setApiEndpoint(input){
@@ -117,7 +159,7 @@ export default function Home() {
       {
         map &&
         <Board
-          onWin={setWin}
+          onWin={winGame}
           map={map}
           newPlayerPosition={playerNewPosition}
           childFunc={childFunc}
@@ -145,8 +187,9 @@ export default function Home() {
       {
         win &&
           <WinOverlay
-            win={win}
-            onPlayAgain={resetGame}
+            wonAt={wonAt}
+            peerId={peerId}
+        //    onPlayAgain={resetGame}
           />
       }
       {
@@ -154,16 +197,10 @@ export default function Home() {
         <WebSockerHandler
           apiEndpoint={apiEndpoint}
           apiToken={apiToken}
-          onMessage={(input)=>{
-            console.log('WS: onMessage', input);
-            set_remotePos1(JSON.parse(input).postion);
-          }}
+          onMessage={gotWsMessage}
         />
       }
-      {/* <p onClick={()=>{fakeRemote([0,1])}}>Press 0,1</p>
-      <p onClick={()=>{fakeRemote([1,1])}}>Press 1,1</p> */}
 
-      {/* <img src={ReactLogo} className="App-logo " alt="logo" /> */}
     </div>
   );
 }
